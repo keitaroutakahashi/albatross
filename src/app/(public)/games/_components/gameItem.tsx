60e30853bde1e6b6d858e2047bf90028e76cdc7c
k-taro <my.address.d@gmail.com>
2026-01-10 15:39:07 +0900

    feat: add game data and formatting utilities for game display

--- src/app/(public)/games/_components/gameItem.tsx
+++ src/app/(public)/games/_components/gameItem.tsx
@@ -1,17 +1,26 @@
 import Image from "next/image";
+import {
+  formatAsMDWithColon,
+  formatAsYYYY,
+  formatToShortDayNameEn,
+} from "@/app/_utils/date/date";
 
-export const GameItem = () => {
+export const GameItem = ({ game }) => {
   return (
     <li className="flex flex-col md:flex-row border border-gray-300 rounded overflow-hidden hover:border-black transition cursor-pointer">
-      <div className="bg-gray-100 p-3 md:p-4 flex md:flex-col justify-between items-center">
+      <div className="bg-gray-100 p-3 md:p-4 flex md:flex-col md:w-64 justify-between items-center">
         <div className="flex items-center gap-x-1">
-          <p className="text-3xl font-bold">10.1</p>
+          <p className="text-3xl font-bold">
+            {formatAsMDWithColon(game.gameDate)}
+          </p>
           <div className="">
-            <p className="text-xs text-gray-500 font-bold">WED</p>
-            <p className="text-xs font-bold">2025</p>
+            <p className="text-xs text-gray-500 font-bold">
+              {formatToShortDayNameEn(game.gameDate)}
+            </p>
+            <p className="text-xs font-bold">{formatAsYYYY(game.gameDate)}</p>
           </div>
         </div>
-        <p className="font-bold text-sm">サタデーリーグ</p>
+        <p className="font-bold text-sm">{game.leagueName}</p>
       </div>
 
       <div className="grid grid-cols-3 p-3 md:px-10 md:flex-1">
@@ -31,18 +40,18 @@ export const GameItem = () => {
           </div>
         </div>
         <div className="flex flex-col items-center justify-center">
-          <p className="text-sm font-bold">東京ドーム</p>
+          <p className="text-sm font-bold">{game.venue}</p>
           <div className="">
-            <span className="text-4xl font-bold">5</span>
+            <span className="text-4xl font-bold">{game.teamScore}</span>
             <span className="text-4xl font-bold mx-2">-</span>
-            <span className="text-4xl font-bold">3</span>
+            <span className="text-4xl font-bold">{game.opposingTeamScore}</span>
           </div>
         </div>
         <div className="flex flex-col items-center justify-center gap-y-2">
           <div className="size-9 bg-gray-300 rounded flex justify-center items-center font-bold">
             A
           </div>
-          <span className="font-bold">Albatross</span>
+          <span className="font-bold">{game.opposingTeam}</span>
         </div>
       </div>
     </li>
--- src/app/(public)/games/_components/gameList.tsx
+++ src/app/(public)/games/_components/gameList.tsx
@@ -1,10 +1,12 @@
 import { GameItem } from "@/app/(public)/games/_components/gameItem";
+import { data } from "@/app/(public)/games/_dummy/data";
 
 export const GameList = () => {
   return (
     <ul className="space-y-4">
-      <GameItem />
-      <GameItem />
+      {data.map((game) => (
+        <GameItem key={game.id} game={game} />
+      ))}
     </ul>
   );
 };
