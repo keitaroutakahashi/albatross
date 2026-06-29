8ed01e9654defa18d8ea1fa4d6103849ff1969a6
k-taro <my.address.d@gmail.com>
2026-06-30 01:29:47 +0900

    add code jump plugin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+import { codeInspectorPlugin } from "code-inspector-plugin";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
+  turbopack: {
+    rules: codeInspectorPlugin({
+      bundler: "turbopack",
+    }),
+  },
 };
 
 export default nextConfig;
